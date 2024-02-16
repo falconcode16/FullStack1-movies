a2b579677c810b99751e2cd8b1a730518aacbc4d
Rohit Patil <rohitpatil1625@gmail.com>
2024-02-16 23:14:14 +0530

    Initial commit

---
+++ src/main/java/dev/rohit16/movies/Movie.java
@@ -0,0 +1,30 @@
+package dev.rohit16.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.bson.types.ObjectId;
+
+import java.util.List;
+
+@Document(collation = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+
+    private String trialerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+
+
+
+ }
---
+++ src/test/java/dev/rohit16/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rohit16.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
